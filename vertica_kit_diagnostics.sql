eed0eeb879e9f2cd96ed121742ac9c138dbcb77a
qinchaofeng <18637156237@163.com>
2016-10-14 18:58:39 +0800

    Update vertica_kit_diagnostics.sql

--- vertica_kit_diagnostics.sql
+++ vertica_kit_diagnostics.sql
@@ -481,3 +481,53 @@ FROM   v_monitor.query_profiles
 GROUP  BY query 
 ORDER  BY avg_query_duration_us DESC 
 LIMIT  50;
+
+---备份资源池
+
+SELECT    'CREATE RESOURCE POOL ' || name
+        || CASE WHEN memorysize                IS NULL THEN ' ' ELSE ' MEMORYSIZE '                 || '''' || memorysize               || '''' END
+        || CASE WHEN maxmemorysize = ''                THEN ' ' ELSE ' MAXMEMORYSIZE '              || '''' || maxmemorysize            || '''' END
+        || CASE WHEN executionparallelism     = 'AUTO' THEN ' ' ELSE ' EXECUTIONPARALLELISM '       || '''' || executionparallelism     || '''' END
+        || CASE WHEN NULLIFZERO(priority)      IS NULL THEN ' ' ELSE ' PRIORITY '                   || '''' || priority                 || '''' END
+        || CASE WHEN runtimepriority           IS NULL THEN ' ' ELSE ' RUNTIMEPRIORITY '            ||         runtimepriority                  END
+        || CASE WHEN runtimeprioritythreshold  IS NULL THEN ' ' ELSE ' RUNTIMEPRIORITYTHRESHOLD '   ||         runtimeprioritythreshold         END
+        || CASE WHEN queuetimeout              IS NULL THEN ' ' ELSE ' QUEUETIMEOUT '               ||         queuetimeout                     END
+        || CASE WHEN maxconcurrency            IS NULL THEN ' ' ELSE ' MAXCONCURRENCY '             ||         maxconcurrency                   END
+        || CASE WHEN runtimecap                IS NULL THEN ' ' ELSE ' RUNTIMECAP '                 || '''' || runtimecap               || '''' END
+        || ' ; '
+FROM v_catalog.resource_pools
+WHERE NOT is_internal
+ORDER BY name;
+
+--备份角色
+
+-- Roles
+------------------------------------------------------------
+SELECT '-- Create Roles';
+SELECT 'CREATE ROLE ' || name || ' ;' AS TXT_CR
+FROM v_catalog.roles
+WHERE name NOT IN ('public','dbadmin','pseudosuperuser','dbduser')
+ORDER BY 1;
+
+------------------------------------------------------------
+-- Add users to Roles
+------------------------------------------------------------
+SELECT '-- Add users to roles';
+SELECT 'GRANT ' || all_roles || ' TO ' || user_name || ';'
+FROM v_catalog.users
+WHERE user_name NOT IN ('dbadmin')
+ORDER BY 1;
+
+--备份schema
+SELECT '-- Create Schema';
+SELECT 'CREATE SCHEMA ' || schema_name  ||  ';'
+FROM schemata
+WHERE schema_name NOT IN ('v_internal','v_catalog','v_monitor','TxtIndex')
+ORDER BY 1;
+--备份用户
+
+SELECT '-- Create Users';
+SELECT 'CREATE USER ' || user_name  || ' RESOURCE POOL ' || resource_pool ||  ' ;'
+FROM v_catalog.users
+WHERE user_name NOT IN ('dbadmin')
+ORDER BY 1;
